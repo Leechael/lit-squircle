e96a51a6f532e9d5bc143e3a44858d5171aa8e56
Leechael Yim <yanleech@gmail.com>
2023-10-02 23:28:50 +0800

    auto sizing on first render

--- src/my-element.ts
+++ src/my-element.ts
@@ -23,18 +23,45 @@ export class Squircle extends LitElement {
   @property({type: String})
   fill: string = 'yellow';
 
+  _width = 0;
+  _height = 0;
+
+  recalcChildrenRect() {
+    let width = 0
+    let height = 0
+    Array.of(...this.children).forEach(el => {
+      const rect = el.getBoundingClientRect()
+      width += rect.width
+      height += rect.height
+    })
+    console.log('recalc', width, height)
+    return [width, height]
+  }
+
+  protected firstUpdated() {
+    const [width, height] = this.recalcChildrenRect()
+    this._width = width
+    this._height = height
+    this.requestUpdate()
+  }
+
   render() {
     const dots = getSvgPath({
-      width: 160,
-      height: 160,
+      width: this._width,
+      height: this._height,
       cornerRadius: this.radius,
       cornerSmoothing: this.cornerSmoothing,
     })
+    console.log('call render', this._width, this._height)
+    let override = ''
+    if (this._height && this._width) {
+      override = `width: ${this._width}px; height: ${this._height}px;`
+    }
     // const baseRadius = 20;
     return html`
       <div
-        class="relative w-40 h-40"
-        style="border-radius: ${this.radius * 1.05}px; background-color: ${this.fill};"
+        class="relative"
+        style="border-radius: ${this.radius * 1.05}px; background-color: ${this.fill}; ${override}"
       >
         <svg class="absolute top-0 left-0 right-0 bottom-0 z-[-1]">
           <path d="${dots}" fill="${this.fill}"></path>
